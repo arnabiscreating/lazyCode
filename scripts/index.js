fa742b7c3b58e0405baa922843abfa24cacc1fb9
Sanket2055 <sanketdiwate95@gmail.com>
2021-10-06 17:25:25 +0530

    added the homepage

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+const cf =document.querySelector(".cf")
+const lc =document.querySelector(".lc");
+const hr =document.querySelector(".hr");
+const cc =document.querySelector(".cc");
+const mis =document.querySelector(".mis");
+const corner=document.querySelector(".corner");
+cf.addEventListener("click" , ()=>{
+    window.location.href = "https://codeforces.com/";
+})
+lc.addEventListener("click" , ()=>{
+    window.location.href = "https://leetcode.com/";
+})
+hr.addEventListener("click" , ()=>{
+    window.location.href = "https://hackerrank.com/";
+})
+cc.addEventListener("click" , ()=>{
+    window.location.href = "https://codechef.com/";
+})
+cf.addEventListener("click" , ()=>{
+    window.location.href = "#";
+})
+corner.addEventListener("click" , ()=>{
+    window.location.href="https://www.google.com/";
+})
