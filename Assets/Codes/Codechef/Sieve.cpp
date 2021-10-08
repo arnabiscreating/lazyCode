c393c17914c872449675bd0ed203a622fa0eff50
Keshab Kataruka <83745415+Keshab0100@users.noreply.github.com>
2021-10-08 16:53:49 +0530

    Create Sieve.cpp

---
+++ Assets/Codes/Codechef/Sieve.cpp
@@ -0,0 +1,29 @@
+// Sieve of Eratosthenes
+#include<iostream>
+using namespace std;
+ 
+void Primesieve(int n){
+    int prime[n]={0};
+    for(int i=2;i<=n;i++){
+        if(prime[i]==0){
+            for (int j = i*i; j <=n; j +=i)
+            {
+                prime[j]=1;
+            }
+            
+        }
+    }
+
+    for(int i=2;i<=n;i++){
+        if(prime[i]==0){
+            cout<<i<<" ";
+        }
+    }cout<<endl;
+    
+} 
+int main(){
+    int n;
+    cin>>n;
+    Primesieve(n);
+    return 0;
+}
