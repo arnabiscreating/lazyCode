ff5a1822c0e5176c6cf3bc11b40c158625621c02
ZaidMarrie <zaidmarrie27@gmail.com>
2021-10-22 14:40:55 +0200

    add navigation menu for mobile

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,1 +1,13 @@
+const menu = document.querySelector('.nav-child2');
+const menuToggle = document.querySelector('.nav-child3');
+const menuToggleText = document.querySelector('.nav-child3 a');
 
+menuToggle.addEventListener('click', function() {
+    menu.classList.toggle('open');
+    
+    if (menu.classList.contains('open')) {
+        menuToggleText.innerHTML = `closeMenu()<span class="menu-state">×</span>`;
+    } else {
+        menuToggleText.innerHTML = `closeMenu()<span class="menu-state">>_</span>`;
+    }
+});
