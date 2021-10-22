c7c7d9db8bbc9251a0c4497e3a28d9378036ec08
ZaidMarrie <zaidmarrie27@gmail.com>
2021-10-22 15:26:43 +0200

    fix failing checks

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,10 +1,12 @@
+'use strict';
+
 const menu = document.querySelector('.nav-child2');
 const menuToggle = document.querySelector('.nav-child3');
 const menuToggleText = document.querySelector('.nav-child3 a');
 
-menuToggle.addEventListener('click', function() {
+menuToggle.addEventListener('click', () => {
     menu.classList.toggle('open');
-    
+
     if (menu.classList.contains('open')) {
         menuToggleText.innerHTML = `closeMenu()<span class="menu-state">×</span>`;
     } else {
